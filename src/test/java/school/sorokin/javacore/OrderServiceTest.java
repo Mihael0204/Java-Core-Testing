5285cbaf343588fc79e698a82029fcbc26929647
Михаил <haasiakira@gmail.com>
2025-05-27 20:34:41 +0300

    Сделаны правки в стиле кода для большей читаемости

--- src/test/java/school/sorokin/javacore/OrderServiceTest.java
+++ src/test/java/school/sorokin/javacore/OrderServiceTest.java
@@ -1,94 +1,90 @@
 package school.sorokin.javacore;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 public class OrderServiceTest {
+
+    private OrderRepository orderRepository;
+    private OrderService orderService;
+
+    @BeforeEach
+    void setUp() {
+        orderRepository = Mockito.mock(OrderRepository.class);
+        orderService = new OrderService(orderRepository);
+    }
 
     @Test
     public void testProcessOrder_Successful() {
-        OrderRepository orderRepositoryMock = Mockito.mock(OrderRepository.class);
-        OrderService orderService = new OrderService(orderRepositoryMock);
         Order order = TestHelper.getDefaultOrder();
-        Mockito.when(orderRepositoryMock.saveOrder(order)).thenReturn(1);
+        Mockito.when(orderRepository.saveOrder(order)).thenReturn(1);
 
         String result = orderService.processOrder(order);
 
         assertEquals("Order processed successfully", result);
 
-        Mockito.verify(orderRepositoryMock, Mockito.times(1)).saveOrder(order);
+        Mockito.verify(orderRepository, Mockito.times(1)).saveOrder(order);
     }
 
     @Test
     public void testProcessOrder_Failed() {
-        OrderRepository orderRepositoryMock = Mockito.mock(OrderRepository.class);
-        OrderService orderService = new OrderService(orderRepositoryMock);
         Order order = TestHelper.getDefaultOrder();
-        Mockito.when(orderRepositoryMock.saveOrder(order)).thenThrow(new RuntimeException("Database error"));
+        Mockito.when(orderRepository.saveOrder(order)).thenThrow(new RuntimeException("Database error"));
 
         String result = orderService.processOrder(order);
 
         assertEquals("Order processing failed", result);
 
-        Mockito.verify(orderRepositoryMock, Mockito.times(1)).saveOrder(order);
+        Mockito.verify(orderRepository, Mockito.times(1)).saveOrder(order);
     }
 
     @Test
     public void shouldCorrectlyCalculateTotalPrice() {
-        OrderRepository orderRepositoryMock = Mockito.mock(OrderRepository.class);
-        OrderService orderService = new OrderService(orderRepositoryMock);
         Order order = TestHelper.getOrderForTesting();
-        Mockito.when(orderRepositoryMock.findOrderById(13)).thenReturn(Optional.of(order));
+        Mockito.when(orderRepository.findOrderById(13)).thenReturn(Optional.of(order));
 
         double result = orderService.calculateTotal(13);
 
         assertEquals(300.0, result);
 
-        Mockito.verify(orderRepositoryMock, Mockito.times(1)).findOrderById(13);
+        Mockito.verify(orderRepository, Mockito.times(1)).findOrderById(13);
     }
 
     @Test
     public void shouldCorrectlyCalculateTotalPrice_WhenOrderNotFound() {
-        OrderRepository orderRepositoryMock = Mockito.mock(OrderRepository.class);
-        OrderService orderService = new OrderService(orderRepositoryMock);
-        Order order = TestHelper.getOrderForTesting();
-        Mockito.when(orderRepositoryMock.findOrderById(13)).thenReturn(Optional.empty());
+        Mockito.when(orderRepository.findOrderById(13)).thenReturn(Optional.empty());
 
         double result = orderService.calculateTotal(13);
 
         assertEquals(0.0, result);
 
-        Mockito.verify(orderRepositoryMock, Mockito.times(1)).findOrderById(13);
+        Mockito.verify(orderRepository, Mockito.times(1)).findOrderById(13);
     }
 
     @Test
     public void shouldCorrectlyCalculateTotalPrice_ZeroQuantity() {
-        OrderRepository orderRepositoryMock = Mockito.mock(OrderRepository.class);
-        OrderService orderService = new OrderService(orderRepositoryMock);
         Order order = TestHelper.getZeroQuantityOrder();
-        Mockito.when(orderRepositoryMock.findOrderById(5)).thenReturn(Optional.of(order));
+        Mockito.when(orderRepository.findOrderById(5)).thenReturn(Optional.of(order));
 
         double result = orderService.calculateTotal(5);
 
         assertEquals(0.0, result);
 
-        Mockito.verify(orderRepositoryMock, Mockito.times(1)).findOrderById(5);
+        Mockito.verify(orderRepository, Mockito.times(1)).findOrderById(5);
     }
 
     @Test
     public void shouldCorrectlyCalculateTotalPrice_ZeroPrice() {
-        OrderRepository orderRepositoryMock = Mockito.mock(OrderRepository.class);
-        OrderService orderService = new OrderService(orderRepositoryMock);
         Order order = TestHelper.getZeroPriceOrder();
-        Mockito.when(orderRepositoryMock.findOrderById(5)).thenReturn(Optional.of(order));
+        Mockito.when(orderRepository.findOrderById(5)).thenReturn(Optional.of(order));
 
         double result = orderService.calculateTotal(5);
 
         assertEquals(0.0, result);
 
-        Mockito.verify(orderRepositoryMock, Mockito.times(1)).findOrderById(5);
+        Mockito.verify(orderRepository, Mockito.times(1)).findOrderById(5);
     }
 }
